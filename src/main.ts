18713e83b5b59fb929eb2b0e4bbe77c2e32278f3
ben <x_z_gfree@163.com>
2022-08-05 23:56:41 +0800

    feature: enableCors.

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
   await app.listen(3000);
 }
 bootstrap();

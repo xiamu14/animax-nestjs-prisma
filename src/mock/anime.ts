49d5aad92fabda65d6540980de7bdec7afbdd2a8
ben <x_z_gfree@163.com>
2023-04-15 17:13:34 +0800

    feature: add some feature.

---
+++ src/mock/anime.ts
@@ -0,0 +1,123 @@
+/**
+ * /anime/popular
+ */
+
+export const animePopular = {
+  id: 4,
+  name: 'Overlord Season 4',
+  author: 'xxx',
+  tags: ['Japanses'],
+  release: '2021',
+  score: 9,
+  introduction:
+    'E-Rantel, the capital city of the newly established Sorcerer Kingdom, suffers from a dire shortage of goods. Once a prosperous city known for its trade, it now faces a crisis due to its caution—or even fear—of its king, Ainz Ooal Gown. To make amends, Ainz sends Albedo to the city as a diplomatic envoy.',
+  cover:
+    'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/overload-season4/cover.jpeg',
+  poster: [],
+};
+
+/**
+ * anime/list
+ */
+
+export const animeList = [
+  {
+    id: 4,
+    name: 'Overlord Season 4',
+    author: 'xxx',
+    tags: ['Japanses'],
+    release: '2021',
+    score: 9,
+    introduction:
+      'E-Rantel, the capital city of the newly established Sorcerer Kingdom, suffers from a dire shortage of goods. Once a prosperous city known for its trade, it now faces a crisis due to its caution—or even fear—of its king, Ainz Ooal Gown. To make amends, Ainz sends Albedo to the city as a diplomatic envoy.',
+    cover:
+      'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/overload-season4/cover.jpeg',
+    poster: [],
+    episodes: [
+      {
+        id: 3,
+        animeId: 4,
+        index: 1,
+        cover:
+          'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/overload-season4/episode1-cover.png',
+        videoUrl:
+          'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/overload-season4/episode1.mp4',
+      },
+      {
+        id: 4,
+        animeId: 4,
+        index: 2,
+        cover:
+          'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/overload-season4/episode2-cover.jpg',
+        videoUrl:
+          'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/overload-season4/episode2.mp4',
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: 'Lycoris Recoil',
+    author: 'Aniplex',
+    tags: ['Japanese'],
+    release: '2022',
+    score: 9,
+    introduction:
+      'Takina Inoue, a self-confident high school girl working as a DA member of an all-female taskforce of assassins and spies known as "Lycoris" who are dispatched to eliminate criminals and terrorists in Tokyo.',
+    cover:
+      'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/lycoris-recoil/cover.jpeg',
+    poster: [],
+    episodes: [
+      {
+        id: 5,
+        animeId: 5,
+        index: 1,
+        cover:
+          'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/lycoris-recoil/episode1-cover.jpg',
+        videoUrl:
+          'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/lycoris-recoil/episode1.mp4',
+      },
+      {
+        id: 6,
+        animeId: 5,
+        index: 2,
+        cover:
+          'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/lycoris-recoil/episode2-cover.jpg',
+        videoUrl:
+          'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/lycoris-recoil/episode2.mp4',
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: 'Made in Abyss',
+    author: 'Abisu',
+    tags: ['Japanese'],
+    release: '2022',
+    score: 9.7,
+    introduction:
+      'An orphaned girl named Riko lives in the Belchero Orphanage in the town of Orth. The town surrounds a strange, giant hole descending deep into the earth, which is known as the Abyss. ',
+    cover:
+      'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/made-in-abyss/cover.jpeg',
+    poster: [],
+    episodes: [
+      {
+        id: 7,
+        animeId: 6,
+        index: 1,
+        cover:
+          'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/made-in-abyss/episode1-cover.jpg',
+        videoUrl:
+          'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/made-in-abyss/episode1.mp4',
+      },
+      {
+        id: 8,
+        animeId: 6,
+        index: 2,
+        cover:
+          'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/made-in-abyss/episode2-cover.jpg',
+        videoUrl:
+          'https://animax-1251727615.cos.ap-nanjing.myqcloud.com/made-in-abyss/episode2.mpg4',
+      },
+    ],
+  },
+];
